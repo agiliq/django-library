165c31779240777f6c4b8ec4035afb5308104334
bitaVersion <pure.vintage.skt@gmail.com>
2012-05-15 17:23:29 +0530

    Improved the models to care of some extra data

--- diglib/models.py
+++ diglib/models.py
@@ -8,8 +8,8 @@ class Publisher(models.Model):
 
     name = models.CharField(max_length=200)
     basic_info = models.CharField(max_length=200)
-    number_of_books_owned = models.DecimalField(max_digits=10,
-            decimal_places=0)
+    number_of_books_published = models.DecimalField(max_digits=10,
+            decimal_places=0, default=1)
 
     def __unicode__(self):
         return self.name
@@ -19,9 +19,9 @@ class Author(models.Model):
 
     name = models.CharField(max_length=200)
     basic_info = models.CharField(max_length=200)
-    number_of_books_owned = models.DecimalField(max_digits=10,
-            decimal_places=0)
-
+    number_of_books_authored = models.DecimalField(max_digits=10,
+            decimal_places=0, default=1)
+    
     def __unicode__(self):
         return self.name
 
@@ -31,14 +31,20 @@ class Books(models.Model):
     title = models.CharField(max_length=200)
     number_of_pages = models.DecimalField(max_digits=10,
             decimal_places=0)
-    isbn_number = models.DecimalField(max_digits=9, decimal_places=0)
+    isbn_number_10 = models.DecimalField(max_digits=9, 
+                                   decimal_places=0, null=True, blank=True)
+    isbn_number_13 = models.DecimalField(max_digits=9, 
+                                   decimal_places=0, null=True, blank=True)
+    description = models.TextField()
     author = models.ManyToManyField(Author)
     publisher = models.ManyToManyField(Publisher)
+    small_thumbnail = models.CharField(max_length=500)
+    thumbnail = models.CharField(max_length=500)
     number_of_upvotes = models.DecimalField(max_digits=9,
-            decimal_places=0)
+            decimal_places=0, default=0)
     number_of_downvotes = models.DecimalField(max_digits=9,
-            decimal_places=0)
-    read_by = models.ManyToManyField(User)
+            decimal_places=0, default=0)
+    read_by = models.ManyToManyField(User, null=True, blank=True)
     tags = TagField(null=True, blank=True)
 
     def set_tags(self, tags):
