6b7b47217ca6a1fe9047bc07efb302f386e9ee0f
bitaVersion <pure.vintage.skt@gmail.com>
2012-05-18 16:27:09 +0530

    Made pyflakes compliant

--- diglib/models.py
+++ diglib/models.py
@@ -1,16 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
 from tagging.fields import TagField
 from tagging.models import Tag
 from django.forms import ModelForm
 
+
 class Publisher(models.Model):
 
     name = models.CharField(max_length=200)
     basic_info = models.CharField(max_length=200)
     number_of_books_published = models.DecimalField(max_digits=10,
-                                                    decimal_places=0, default=1)
-
+            decimal_places=0, default=1)
 
     def __unicode__(self):
         return self.name
@@ -21,8 +23,8 @@ class Author(models.Model):
     name = models.CharField(max_length=200)
     basic_info = models.CharField(max_length=200)
     number_of_books_authored = models.DecimalField(max_digits=10,
-                                                   decimal_places=0, default=1)
-    
+            decimal_places=0, default=1)
+
     def __unicode__(self):
         return self.name
 
@@ -31,20 +33,20 @@ class Books(models.Model):
 
     title = models.CharField(max_length=200)
     number_of_pages = models.DecimalField(max_digits=10,
-                                          decimal_places=0)
-    isbn_number_10 = models.DecimalField(max_digits=15, 
-                                         decimal_places=0, null=True, blank=True)
-    isbn_number_13 = models.DecimalField(max_digits=15, 
-                                         decimal_places=0, null=True, blank=True)
+            decimal_places=0)
+    isbn_number_10 = models.DecimalField(max_digits=15,
+            decimal_places=0, null=True, blank=True)
+    isbn_number_13 = models.DecimalField(max_digits=15,
+            decimal_places=0, null=True, blank=True)
     description = models.TextField()
     author = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher, null=True)
     small_thumbnail = models.CharField(max_length=500)
     thumbnail = models.CharField(max_length=500)
     number_of_upvotes = models.DecimalField(max_digits=9,
-                                            decimal_places=0, default=0)
+            decimal_places=0, default=0)
     number_of_downvotes = models.DecimalField(max_digits=9,
-                                              decimal_places=0, default=0)
+            decimal_places=0, default=0)
     read_by = models.ManyToManyField(User, null=True, blank=True)
 
     tags = TagField(null=True, blank=True)
@@ -59,13 +61,12 @@ class Books(models.Model):
     def __unicode__(self):
         return self.title
 
-STATUS_CHOICE = (
-    ('Pending', 'Pending'),
-    ('Approved', 'Approved'),
-    )
+
+STATUS_CHOICE = (('Pending', 'Pending'), ('Approved', 'Approved'))
+
 
 class BookRequest(models.Model):
-    
+
     usr = models.ForeignKey(User)
     book_title = models.CharField(max_length=400)
     author = models.CharField(max_length=400)
@@ -74,16 +75,21 @@ class BookRequest(models.Model):
     url = models.URLField(max_length=500)
 
     def __unicode__(self):
-        return u'%s by %s' %(self.book_title, self.usr)
+        return u'%s by %s' % (self.book_title, self.usr)
+
 
 class Transaction(models.Model):
-    
+
     usr = models.ForeignKey(User)
     book = models.ForeignKey(Books)
-    date_issued = models.DateField(auto_now=False, auto_now_add=False, null=True)
-    date_returned = models.DateField(auto_now=False, auto_now_add=False, null=True)
+    date_issued = models.DateField(auto_now=False, auto_now_add=False,
+                                   null=True)
+    date_returned = models.DateField(auto_now=False,
+            auto_now_add=False, null=True)
+
 
 class BookRequestForm(ModelForm):
-    
+
     class Meta:
+
         model = BookRequest
--- diglib/views.py
+++ diglib/views.py
@@ -1,81 +1,82 @@
-from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.core.exceptions import ObjectDoesNotExist
-from django.shortcuts import render_to_response, get_object_or_404, redirect
-from django.template import RequestContext
-from django.views.decorators.csrf import csrf_exempt
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from django.views.generic.simple import direct_to_template
-from django.contrib.auth import authenticate
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import logout, login
-from django.conf import settings
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.views.decorators.csrf import csrf_exempt
-from diglib.models import Books, Author, Publisher, Transaction, BookRequestForm, BookRequest
+from diglib.models import Books, Author, Publisher, Transaction, \
+    BookRequestForm, BookRequest
 
 import datetime
 
-model_dict = {'book': Books, 'author':Author, 'publisher':Publisher}
+model_dict = {'book': Books, 'author': Author, 'publisher': Publisher}
 
 
 def about(request):
-    return render(request, "about.html", {})
+    return render(request, 'about.html', {})
 
+
+@login_required
 def edit_request(request, req_id):
     req_data = BookRequest.objects.get(id=req_id)
-    form = BookRequestForm(data = request.POST or None, instance=req_data)
+    form = BookRequestForm(data=request.POST or None, instance=req_data)
     if form.is_valid():
         form.save()
-        return redirect("/request/detail/")
-    return render(request, "request.html", {'form':form})
+        return redirect('/request/detail/')
+    return render(request, 'request.html', {'form': form})
 
+
+@login_required
 def book_request(request, var):
-    
-    if var == "detail":
-        bookreqs = BookRequest.objects.all().order_by('-date_requested')
-        return render(request, "request.html", {'reqs':bookreqs})
 
-    if var == "create":
-        form = BookRequestForm(data = request.POST or None)
+    if var == 'detail':
+        bookreqs = BookRequest.objects.all().order_by('-date_requested')
+        return render(request, 'request.html', {'reqs': bookreqs})
+
+    if var == 'create':
+        form = BookRequestForm(data=request.POST or None)
         if form.is_valid():
             form.save()
-            return redirect("/reques/detail/")
-        return render(request, "request.html", {'form':form})
-        
-    
+            return redirect('/reques/detail/')
+        return render(request, 'request.html', {'form': form})
+
+
 def aclogout(request):
     logout(request)
-    return redirect("/")
+    return redirect('/')
 
 
+@login_required
 @csrf_exempt
 def funct(request, book_id, var):
-    book = Books.objects.get(id = book_id)
-    if var == "issue" and book.status == False:
-        return HttpResponse("0")
-    elif var == "return" and book.status == True:
-        return HttpResponse("0")
+    book = Books.objects.get(id=book_id)
+    if var == 'issue' and book.status == False:
+        return HttpResponse('0')
+    elif var == 'return' and book.status == True:
+        return HttpResponse('0')
     else:
-        if var == "upvote":
+        if var == 'upvote':
             book.number_of_upvotes += 1
-        if var == "downvote":
+        if var == 'downvote':
             book.number_of_downvotes += 1
-        
+
         usr = request.user
-    
-        if var == "issue":
-            tr = Transaction.objects.create(usr=usr,
-                                        book=book,
-                                        date_issued=datetime.date.today())
+
+        if var == 'issue':
+            tr = Transaction.objects.create(usr=usr, book=book,
+                    date_issued=datetime.date.today())
             book.status = False
-        if var == "return":
-            tr = Transaction.objects.filter(usr = usr, 
-                                            book = book).order_by('-date_issued')[0]
-            tr.date_returned=datetime.date.today()
+            tr.save()
+        if var == 'return':
+            tr = Transaction.objects.filter(usr=usr,
+                    book=book).order_by('-date_issued')[0]
+            tr.date_returned = datetime.date.today()
             book.read_by.add(usr)
             book.status = True
-        tr.save()
+            tr.save()
         book.save()
         return HttpResponse(var)
 
@@ -89,35 +90,40 @@ def aclogin(request):
         if user is not None:
             if user.is_active:
                 login(request, user)
-                return redirect("/")
+                return redirect('/')
             else:
-                return HttpResponse("disabled account")
+                return HttpResponse('disabled account')
         else:
-            return HttpResponse("invalid login")
+            return HttpResponse('invalid login')
     else:
-        return render(request, "login.html", {}) 
+        return render(request, 'login.html', {})
+
 
 def filter(request, var_name=None, var_id=None):
-    if var_name == "author":
-        obj = Books.objects.filter(author__id = var_id)
+    if var_name == 'author':
+        obj = Books.objects.filter(author__id=var_id)
     else:
-        obj = Books.objects.filter(publisher__id = var_id)
-    return render(request, 'filter.html', {'books':obj})
+        obj = Books.objects.filter(publisher__id=var_id)
+    return render(request, 'filter.html', {'books': obj})
+
 
 def libdetails(request):
     tr = Transaction.objects.all().order_by('-date_issued')[:20]
-    return render(request, "trans.html", {"trans":tr})
+    return render(request, 'trans.html', {'trans': tr})
+
 
 def index(request):
     books = Books.objects.all()
     return render(request, 'index.html', {'books': books})
 
-def detail(request, var_name=None, var_id=None):
-    model = model_dict.get(var_name, "")
-    obj = get_object_or_404(model, pk=var_id)
-    return render(request, 'detail.html', {'detail':var_name, var_name:obj})
 
-    
+def detail(request, var_name=None, var_id=None):
+    model = model_dict.get(var_name, '')
+    obj = get_object_or_404(model, pk=var_id)
+    return render(request, 'detail.html', {'detail': var_name,
+                  var_name: obj})
+
+
 def status(request, book_id=None):
     tr = Transaction.objects.filter(book__id=book_id)
     return render(request, 'status.html', {'status_data': tr})
